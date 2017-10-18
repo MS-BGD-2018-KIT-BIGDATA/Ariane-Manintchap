32faadc3606c7da46713128a263b099737f34d75
Ariane-tchuenteu <amanintchap@telecom-paristech.fr>
2017-10-18 19:07:03 +0200

    exo_cc_lesson_3

--- Lesson 4/exo_cc_lesson_4.py
+++ Lesson 4/exo_cc_lesson_4.py
@@ -1,17 +1,20 @@
-# Connaitre les distances en voiture villes à  villes 
+# Connaitre les distances en voiture villes Ã  villes 
 # dans le but d'implanter des bureaux commerciaux en france
-# récupérer la liste des 100 plus grandes villes
+# rÃ©cuÃ©rer la liste des 100 plus grandes villes
 # puis sortir la matrice des distances
-# API à  utiliser: google distance matrice API
+# API Ã  utiliser: google distance matrice API
 # https://developers.google.com/maps/documentation/distance-matrix/?hl=fr
+# Cyril Nguyen
+# joseph asouline
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+import re
 
 
 url = "https://lespoir.jimdo.com/2015/03/05/classement-des-plus-grandes-villes-de-france-source-insee/"
+url1 = "https://developers.google.com/maps/documentation/distance-matrix/?hl=fr"
 
 def getSoupFromURL(url, method='get', data={}):
 
@@ -30,10 +33,12 @@ def getSoupFromURL(url, method='get', data={}):
 
 soup = getSoupFromURL(url)
 
+#Api = getSoupFromURL(url1)
+
 def getTowns(url):
     soup = getSoupFromURL(url)
     Towns = []        
-    for i in range(1,101):
+    for i in range(1,10):
         Towns += [soup.find_all("td", class_ ="xl65")[3*i-2].text.strip()]
     return(Towns)
     
@@ -47,8 +52,13 @@ for town1 in towns:
     for town2 in towns:
         url3 = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + town1 + ",FR&destinations=" + town2 +",FR&key=" + key 
         dist = requests.get(url3)
-        distance = dist.json()["rows"][0]['elements'][0]
-        if distance["status"] == "OK":
-            distanceTown.loc[town1,town2] = distance['distance']['text'].split(" ")[0]
+        distanceTown.loc[town1,town2] = dist.json()['rows'][0]['elements'][0]['distance']['text']
+
+
+https://maps.googleapis.com/maps/api/distancematrix/json?origins=Paris&destinations=Nancy
+
+
+
+
 
 
