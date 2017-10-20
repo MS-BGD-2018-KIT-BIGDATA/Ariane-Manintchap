fdf97e02a50912aaa2b0a69272e446f20d0a3b67
Ariane-tchuenteu <amanintchap@telecom-paristech.fr>
2017-10-20 02:00:43 +0200

    exo_dom_lesson_4

--- Lesson 4/exo_dom_lesson_4.py
+++ Lesson 4/exo_dom_lesson_4.py
@@ -16,11 +16,8 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import re
-
-# url_zoe = "https://www.leboncoin.fr/voitures/offres/ile_de_france/?th=1&q=Renault%20Zo%E9"
-# url_prof = "https://www.leboncoin.fr/voitures/offres/ile_de_france/?q=Renault%20Zo%E9&f=c"
-# url_particul = "https://www.leboncoin.fr/voitures/offres/ile_de_france/?q=Renault%20Zo%E9&f=p"
-
+import multiprocessing as mp
+import numpy as np
 
 def getSoupFromURL(url, method='get', data={}):
     if method == 'get':
@@ -54,152 +51,76 @@ def getPageNumber(statu):
 def getVariablesValues(url):
     soup = getSoupFromURL(url)
     val = soup.find_all("span", class_="value")
-    data = []
+    data = {}
     # insertion de la version
-    data.append(soup.find_all("h1", class_="no-border")[0].text.strip())
+    data["version"] = soup.find_all("h1", class_="no-border")[0].text.strip()
     #insertion de l'année
-    data.append(val[4].text.strip())
+    data["Year"] = val[4].text.strip()
     #insertion du kilometrage
-    Kilometrage = val[5].text.strip()
-    data.append(val[5].text.strip())
+    Kilometrage = "".join(re.findall("\d+", val[5].text.strip()))
+    data["Kilometrage"] = int(Kilometrage)
     #insertion du prix
-    data.append(val[0].text.strip())
+    price = "".join(re.findall("\d+", val[0].text.strip()))
+    data["price"] = int(price)
     #insertion du statu
     return data
     
 statu = ["c", "p"]
-df_Ile_de_france = pd.DataFrame(columns=["version", "année", "kilométrage", "prix", "statu_propri"])
+result = []
 
-version = []
-Year= []
-kilo= []
-price = []
-statu_prop = []
+def ExtratVariablesParticulier(url):
+    m = "https:" + url
+    don = getVariablesValues(m)
+    don["statu_propi"] = "c"
+    return don
+
+def ExtratVariablesProffessionel(url):
+    m = "https:" + url
+    don = getVariablesValues(m)
+    don["statu_propi"] = "p"
+    return don   
 
 for elt in statu:
     if elt == "c":
         n = getPageNumber(elt)
         for i in range(1,n):
-            urlList = getListOfUrlCar(elt,i)
-            for url1 in urlList:
-                m = "https:" + url1
-                don = getVariablesValues(m)
-                version.append(don[0])
-                Year.append(don[1])
-                kilo.append(don[2])
-                price.append(don[3])
-                statu_prop.append(elt)
+            urlList = getListOfUrlCar(elt,i)    
+            pool = mp.Pool(processes =10)
+            don = pool.map(ExtratVariablesParticulier, urlList)#renvoit un tableau d'elements
+            result.extend(don)
     else:
         url3 = "https://www.leboncoin.fr/voitures/offres/ile_de_france/?q=Renault%20Zo%E9&f=p" 
         soup = getSoupFromURL(url3)
         class_result = "list_item clearfix trackable"
-        results_search = []
-        results_search += [a['href'] for a in soup.find_all("a", class_= class_result)]
-        urlList = results_search
-        for url1 in urlList:
-            m = "https:" + url1
-            don = getVariablesValues(m)
-            version.append(don[0])
-            Year.append(don[1])
-            kilo.append(don[2])
-            price.append(don[3])
-            statu_prop.append(elt)
-    
-df_Ile_de_france["version"] = version    
-df_Ile_de_france["année"] = Year
-df_Ile_de_france["kilométrage"] = kilo
-df_Ile_de_france["prix"] = price
-df_Ile_de_france["statu_propri"] = statu_prop
+        urlList = []
+        urlList += [a['href'] for a in soup.find_all("a", class_= class_result)]
+        pool = mp.Pool(processes =10)
+        don = pool.map(ExtratVariablesProffessionel, urlList)#renvoit un tableau d'elements
+        result.extend(don)
+            
+df_Ile_de_france = pd.DataFrame(result)
+df_Ile_de_france["version"] = df_Ile_de_france.version.str.lower()
 
+def split_it(u):
+    result = re.findall("(fuence|zoé|zoe|zen|intens|life){1}", u)
+    result.reverse()
+    return result[0]
 
+df_Ile_de_france["version"] = df_Ile_de_france["version"].apply(split_it) 
+df_Ile_de_france = df_Ile_de_france.replace(to_replace= 'ze', value='zen').replace(to_replace= 'zoé', value='zen').replace(to_replace= 'fuence', value='zen').replace(to_replace= 'zoe', value='zen')
+d = df_Ile_de_france.groupby("Year").first()  
+n = df_Ile_de_france.shape[0]
+df_Ile_de_france["Argus"] = np.zeros(n)
 
+for year in d.index:
+    df2 = df_Ile_de_france[df_Ile_de_france.Year == year]
+    df2 = df2.groupby("version").first()
+    for vers in df2.index:
+        url6 = "https://www.lacentrale.fr/cote-auto-renault-zoe-" + vers + "+charge+rapide-" + year + ".html"
+        soup6 = getSoupFromURL(url6)
+        Argus = soup6.find_all("span", class_= "jsRefinedQuot")[0].text
+        Argus = int("".join(re.findall("\d+", Argus)))
+        df_Ile_de_france["Argus"][(df_Ile_de_france.Year == year) & (df_Ile_de_france.version == vers)] = Argus
 
-
-
-
-
-
-
-
-
-
-def getPrice(url):
-    soup = getSoupFromURL(url)
-    return soup.find_all("span", class_="value")[0].text.strip()
-
-def getYear(url):
-    soup = getSoupFromURL(url)
-    return soup.find_all("span", class_="value")[4].text.strip()
-
-def getKilometrage(url):
-    soup = getSoupFromURL(url)
-    Kilometrage = soup.find_all("span", class_="value")[5].text.strip()
-    return re.findall("\d+", Kilometrage)
-
-
-def getVersion(url):
-    soup = getSoupFromURL(url)
-    return soup.find_all("h1", class_="no-border")[0].text.strip()
-
-
-age = re.findall("\d{2}", "Ariane a 22 ans")
-
-
-#re.findall("\d+", '15 254 KM')
-
-
-url = "https://www.leboncoin.fr/voitures/offres/ile_de_france/?o=1&q=Renault%20Zo%E9&f=c"
-soup = getSoupFromURL(url)
-soup.find_all("a", class_="element page static")[1]["href"]
-soup.find_all("a", {"id": "last"}, class_="element page static")[0]["href"]
-u = soup.find_all("a", class_="element page")
-
-
-soup.find_all("a", {"id": "last"})
-
-<a href="//www.leboncoin.fr/voitures/offres/ile_de_france/?o=3&amp;q=Renault%20Zo%E9&amp;f=c" class="element page static" id="last"><i class="icon-chevron-double-right nomargin"></i></a>
-
-
-https://www.lacentrale.fr/cote-voitures-renault-zoe--2013-.html
-https://www.lacentrale.fr/cote-auto-renault-zoe-intens+charge+rapide-2014.html
-https://www.lacentrale.fr/cote-auto-renault-zoe-zen+charge+rapide-2014.html
-https://www.lacentrale.fr/cote-auto-renault-zoe-zen+charge+rapide-2014.html
-
-#############################################################################à
-url = 'https://gist.github.com/paulmillr/2657075'
-
-
-
-soup = getSoupFromURL(url)
-
-#rows = soup.find_all("th", scope = "row")
-#for row in rows:
-#    print(row.parent.text + " " + row.parent.td.text)
-
-def getUserNames(url):
-    soup = getSoupFromURL(url)
-    Usernames = []        
-    for i in range(0,256):
-        Usernames += [soup.find_all("th", scope = "row")[i].parent.a.text]
-    return(Usernames)
-        
-
-Usernames = getUserNames(url)    
-for i in range(0,256):
-    print('#' + str(i+1) + ' ' + Usernames[i])
-
-def getNomberMeanofStarUser(user):
-    #url_search = "https://github.com/" + user
-    url_search = "https://api.github.com/users/" + user + "/repos"
-    res = requests.get(url_search) 
-    if res.status_code == 200:
-        mean = pd.Series([rep['stargazers_count'] for rep in res.json()]).mean()
-        return print("Le nombre moyen de star des repositories de l user " + user +" est " + str(mean))   
-    else:
-        return 'url wrong response'
-    
-for user in Usernames:
-    print(getNomberMeanofStarUser(user))   
- 
-
+df_Ile_de_france.to_csv('df_Ile_de_france.csv')
 
